ae591de1d1edc91fe49bf006378a609ff4c007e5
Dan Tisdale <dant.grnt@gmail.com>
2025-10-12 15:55:58 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js (ESM)
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
