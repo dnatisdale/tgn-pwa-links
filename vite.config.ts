8b21edee4beda7eb267b4bedbdd9b58cd723d971
dnatisdale <dant.grnt@gmail.com>
2025-10-30 12:26:00 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
 
       // Keep SW off in dev to avoid port/cache traps
-      devOptions: { enabled: false },
+      devOptions: { enabled: true },
 
       // Precache self-hosted fonts + CSS (served from /public)
       includeAssets: [
