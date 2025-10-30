e4e3683489148db99d0e96b1c38dc6d7392e2902
dnatisdale <dant.grnt@gmail.com>
2025-10-30 12:03:30 -0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
-/// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+// src/env.d.ts
+/// <reference types="vite/client" />
+
+declare const __APP_VERSION__: string;
+declare const __BUILD_TIME_PST__: string;
+declare const __BUILD_ID__: string;
+declare const __BUILD_DATE__: string;
+declare const __BUILD_TIME__: string;
+declare const __BUILD_PRETTY__: string;
