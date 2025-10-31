fe7b0cfc3b72e91b933f630671fc464f60a69d30
dnatisdale <dant.grnt@gmail.com>
2025-10-31 03:27:01 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx,html}'],
   theme: {
     extend: {
+      fontFamily: {
+        // This generates the utility class: `font-krub`
+        krub: ['"Krub"', 'system-ui', 'sans-serif'],
+      },
       colors: {
         thaiRed: '#A51931',
         thaiBlue: '#2D2A4A',
